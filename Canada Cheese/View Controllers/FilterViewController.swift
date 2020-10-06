35f37c7250fc1f6ab0b8c28331f7148a1d0aa131
taylor-young0 <59883690+taylor-young0@users.noreply.github.com>
2020-10-05 20:57:59 -0400

    - Adds the manufacturer province to the filters view controller - All the cells on the filter screen are now correctly labeled - These cheese filter cells now toggle a checkmark when tapped

--- Canada Cheese/View Controllers/FilterViewController.swift
+++ Canada Cheese/View Controllers/FilterViewController.swift
@@ -11,12 +11,13 @@ import UIKit
 class FilterViewController: UITableViewController {
 
     var filters = [
-        "Manufacturing type": ["Artisan", "Farmstead", "Industrial"],
-        "Organic": ["Organic", "Non-organic"],
-        "Cheese type": ["Soft Cheese", "Semi-soft Cheese", "Firm Cheese", "Hard Cheese", "Veined Cheese", "Fresh Cheese"],
-        "Milk type": ["Ewe", "Ewe and Goat", "Goat", "Cow", "Cow and Goat", "Buffalo Cow"],
-        "Milk treatment": ["Raw Milk", "Pasteurized", "Thermised"],
-        "Rind type": ["Washed Rind", "Brushed Rind", "No Rind"]
+        0 : ["Manufacturing type": ["Artisan", "Farmstead", "Industrial"]],
+        1 : ["Manufacturer province" : ["BC", "AB", "SK", "MB", "ON", "QC", "NL", "NB", "NS", "PE"]],
+        2 : ["Organic": ["Organic", "Non-organic"]],
+        3 : ["Cheese type": ["Soft Cheese", "Semi-soft Cheese", "Firm Cheese", "Hard Cheese", "Veined Cheese", "Fresh Cheese"]],
+        4 : ["Milk type": ["Ewe", "Ewe and Goat", "Goat", "Cow", "Cow and Goat", "Buffalo Cow"]],
+        5 : ["Milk treatment": ["Raw Milk", "Pasteurized", "Thermised"]],
+        6 : ["Rind type": ["Washed Rind", "Brushed Rind", "No Rind"]]
     ]
     
     override func viewDidLoad() {
@@ -46,14 +47,25 @@ class FilterViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return filters.count
+        return (filters[section]?.first?.value.count)!
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "filterCell", for: indexPath) as! CheeseFilterCell
-        cell.filter.text = filters["Organic"]![0]
+        cell.filter.text = filters[indexPath.section]!.first?.value[indexPath.row]
+        cell.accessoryType = .none
 
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return filters[section]?.first?.key
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath) as! CheeseFilterCell
+        let currentAccessory = cell.accessoryType
+        cell.accessoryType = (currentAccessory == .none ? .checkmark : .none)
     }
 
     /*
